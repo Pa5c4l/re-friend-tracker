c78f4bb4fb42b268148698d998f2a2f9323be598
Pa5c4l <pascal.stoerrlein@gmail.com>
2024-05-10 08:00:08 +0000

    title

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "ZHAW Requirements Engineering Friend Tracker (FS2024)",
+            "title": "RE Re-Friend Tracker, Störrlein Pascal",
             "formList": [
                 {
                     "id": "FriendForm",
